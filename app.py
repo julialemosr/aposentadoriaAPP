90560558bc86fae3adefd8b4f2e9c3d24052c9b0
julialemosr <julialemosr20@gmail.com>
2025-04-17 11:25:50 -0300

    aposentadoria app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flet as ft
 from flet import AppBar, ElevatedButton, Page, Text, colors
 from flet.core.colors import Colors
 from flet.core.dropdown import Option
+from flet.core.types import CrossAxisAlignment, MainAxisAlignment
 from flet.core.view import View
 
 
@@ -18,28 +19,37 @@ def main(page: ft.Page):
         page.views.append(
             View(
                 "/",
-                [
+                (
+                    ft.Container(
+                        ft.Image(src='inss.png', width=300, height=300),
+                    ),
+                        AppBar(title=Text(""), bgcolor=Colors.INDIGO_900),
+                        ElevatedButton(text="Simulador de aposentadoria", on_click=lambda _: page.go("/segunda")),
+                        ElevatedButton(text="Ver regras do simulador", on_click=lambda _: page.go("/terceira")),
 
-                    ElevatedButton(text="Simulador de aposentadoria", on_click=lambda _: page.go("/segunda")),
-                    ElevatedButton(text="Ver regras do simulador", on_click=lambda _: page.go("/terceira")),
 
-                ],
+                ),
+                bgcolor=ft.Colors.INDIGO_900,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
             )
+
         )
         if page.route == "/segunda":
             page.views.append(
                 View(
                     "/segunda",
                     [
-                        AppBar(title=Text("Simulador"), bgcolor=Colors.SECONDARY),
-                        Text(f"SIMULADOR:"),
-                        input_idade,
-                        sexo,
-                        input_contribuicao,
-                        input_media_salarial,
-                        categoria,
-                        ElevatedButton(text="Simular aposentadoria", on_click= genero)
-                    ]
+                            AppBar(title=Text(""), bgcolor=Colors.INDIGO_900),
+                            Text(theme_style=ft.TextThemeStyle.TITLE_MEDIUM, value="SIMULADOR DE APOSENTADORIA:\n"),
+                            input_idade,
+                            sexo,
+                            input_contribuicao,
+                            input_media_salarial,
+                            categoria,
+                            ElevatedButton(text="Simular aposentadoria", on_click= genero)
+                    ],
+                    bgcolor=ft.Colors.INDIGO_900,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
                 )
             )
 
@@ -48,18 +58,21 @@ def main(page: ft.Page):
                 View(
                     "/terceira",
                     [
-                        AppBar(title=Text("Regras"), bgcolor=Colors.TERTIARY),
-                        Text(f"REGRAS DO SIMULADOR:\n"),
-                        Text(f"Aposentadoria por Idade:\n"),
-                        Text(f"Homens: 65 anos de idade e pelo menos 15 anos de contribuição.\n"),
-                        Text(f"Mulheres: 62 anos de idade e pelo menos 15 anos de contribuição.\n"),
-                        Text(f"Aposentadoria por Tempo de Contribuição:\n"),
-                        Text(f"Homens: 35 anos de contribuição.\n"),
-                        Text(f"Mulheres: 30 anos de contribuição.\n"),
-                        Text(f"Valor Estimado do Benefício:\n"),
-                        Text(
-                            f" O valor da aposentadoria será uma média de 60% da média salarial informada, acrescido de 2% por ano que exceder o tempo mínimo  de contribuição. "),
-                    ]
+                            AppBar(title=Text(""), bgcolor=Colors.INDIGO_900),
+                            Text(theme_style=ft.TextThemeStyle.TITLE_MEDIUM, value="REGRAS DO SIMULADOR:\n"),
+                            Text(f"Aposentadoria por Idade:"),
+                            Text(f"Homens: 65 anos de idade e pelo menos 15 anos de contribuição."),
+                            Text(f"Mulheres: 62 anos de idade e pelo menos 15 anos de contribuição.\n"),
+                            Text(f"Aposentadoria por Tempo de Contribuição:"),
+                            Text(f"Homens: 35 anos de contribuição."),
+                            Text(f"Mulheres: 30 anos de contribuição.\n"),
+                            Text(f"Valor Estimado do Benefício:"),
+                            Text(
+                                f" O valor da aposentadoria será uma média de 60% da média salarial informada, acrescido "
+                                f"de 2% por ano que exceder o tempo mínimo  de contribuição. "),
+                    ],
+                    bgcolor=ft.Colors.INDIGO_900,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
                 )
             )
 
@@ -68,10 +81,15 @@ def main(page: ft.Page):
                 View(
                     "/quarta",
                     [
-                        AppBar(title=Text("Resultado da simulação"), bgcolor=Colors.TERTIARY),
-                        Text(f"RESULTADO:\n"),
-                        txt_resultado
-                    ]
+                        ft.Container(
+                            ft.Image(src='inss.png', width=200, height=200),
+                        ),
+                            AppBar(title=Text(""), bgcolor=Colors.INDIGO_900),
+                            Text(f"RESULTADO:\n"),
+                            txt_resultado
+                    ],
+                    bgcolor=ft.Colors.INDIGO_900,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
                 )
             )
 
@@ -119,7 +137,7 @@ def main(page: ft.Page):
             # calc fem
             if categoria.value == 'Idade':
                 if sexo.value == 'Fem':
-                    if int(input_idade.value) >= 62 and input_contribuicao.value >= 15:
+                    if int(input_idade.value) >= 62 and int (input_contribuicao.value) >= 15:
                         aposentadoria_i = (float(input_media_salarial.value) * 0.6)
                         txt_resultado.value = f'Você já pode se aposentar e irá receber R${aposentadoria_i} de aposentadoria.'
 
@@ -135,7 +153,7 @@ def main(page: ft.Page):
                         txt_resultado.value = f'Você ainda não pode se aposentar, mas poderá se aposentar em {data_aposentadoria}.'
 
                 else:
-                    if int(input_idade.value) >= 65 and input_contribuicao.value >= 15:
+                    if int(input_idade.value) >= 65 and int (input_contribuicao.value) >= 15:
                         aposentadoria_i = (float(input_media_salarial.value) * 0.6)
                         txt_resultado.value = f'Você já pode se aposentar e irá receber R${aposentadoria_i} de aposentadoria.'
 
@@ -194,21 +212,28 @@ def main(page: ft.Page):
         top_view = page.views[-1]
         page.go(top_view.route)
 
-    input_idade = ft.TextField(label='Idade', hint_text='Digite sua idade atual')
+    input_idade = ft.TextField(label='Idade',bgcolor=Colors.BLUE_GREY_900, hint_text='Digite sua idade atual')
     sexo = ft.Dropdown(
-        label='Sexo',
+        label='Sexo',bgcolor=Colors.BLUE_GREY_900,
+        width= 400,
+        filled=True,
+        fill_color=Colors.BLUE_GREY_900,
         options=[
             Option('Masc', 'Masculino'),
             Option('Fem', 'Feminino')
         ],
     )
     input_contribuicao = ft.TextField(label='Anos de contribuicao',
+                                      bgcolor=Colors.BLUE_GREY_900,
                                       hint_text='A quantos anos você contribui para o INSS')
 
-    input_media_salarial = ft.TextField(label='Média salarial', hint_text='Média salarial dos últimos 5 anos')
+    input_media_salarial = ft.TextField(label='Média salarial',bgcolor=Colors.BLUE_GREY_900, hint_text='Média salarial dos últimos 5 anos')
 
     categoria = ft.Dropdown(
         label='Categoria de aposentadoria',
+        bgcolor=Colors.BLUE_GREY_900,
+        filled=True,
+        fill_color=Colors.BLUE_GREY_900,
         options=[
             Option('Contribuicao', 'Aposentadoria por tempo de contribuição'),
             Option('Idade', 'Aposentadoria por idade')
